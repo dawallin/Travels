c4250f5b9211ea2145f162d99c29556fb16e3c78
Daniel Wallin <dawallin@users.noreply.github.com>
2026-01-19 20:40:57 +0100

    Add possible Munduk hotel option

---
+++ src/data/places/bali/puri-alam-bali-munduk.ts
@@ -0,0 +1,11 @@
+export default {
+  id: "puri-alam-bali-munduk",
+  title: "Puri Alam Bali Munduk",
+  type: "hotel",
+  status: "possible",
+  parentDestinationId: "munduk",
+  description:
+    "Bekvämt lodge-hotell nära by/väg, bra som utflyktsbas men mindre immersivt än mer avskilda alternativ.",
+  latitude: -8.2636,
+  longitude: 115.0789,
+};
---
+++ src/pages/trips/bali/places/puri-alam-bali-munduk.md
@@ -0,0 +1,7 @@
+---
+layout: "../../../../layouts/PlaceLayout.astro"
+title: "Puri Alam Bali Munduk"
+---
+Bekvämt lodge-hotell nära by/väg, bra som utflyktsbas men mindre immersivt än mer avskilda alternativ.
+
+- https://www.tripadvisor.com/Hotel_Review-g608491-d1208060-Reviews-Puri_Alam_Bali_Munduk-Munduk_Bali.html
