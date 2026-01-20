59c18335c91221dba66e93cea691ccd2277ad52b
Daniel Wallin <dawallin@users.noreply.github.com>
2026-01-20 19:30:35 +0100

    Add coordinates for Munduk Waterfalls

--- src/data/places/bali/munduk-waterfalls.ts
+++ src/data/places/bali/munduk-waterfalls.ts
@@ -6,4 +6,6 @@ export default {
   parentDestinationId: "munduk",
   description:
     "En klassisk vandringsrunda i Munduk-området som tar dig genom en dal med flera vattenfall, frodig grönska och krydd-/kaffeodlingar. Välj enstaka fall eller gör en längre trek som binder ihop flera.",
+  latitude: -8.2646,
+  longitude: 115.0717,
 };
