b2bce516f9584fdf30b568eb610b37a2b49fb789
Daniel Wallin <dawallin@users.noreply.github.com>
2026-01-20 19:39:29 +0100

    Update Amed coordinates

--- src/data/places/bali/amed.ts
+++ src/data/places/bali/amed.ts
@@ -5,6 +5,6 @@ export default {
   status: "possible",
   parentDestinationId: "bali",
   description: "Coastal village area known for calm bays and views of Mount Agung.",
-  latitude: -8.3466,
-  longitude: 115.559,
+  latitude: -8.3469,
+  longitude: 115.56,
 };
