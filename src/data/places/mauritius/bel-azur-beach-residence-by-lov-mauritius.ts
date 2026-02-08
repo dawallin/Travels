6361cb198cf1b00a734e6c3bc74f7828129d516a
Daniel Wallin <dawallin@users.noreply.github.com>
2026-02-08 20:46:20 +0100

    Add Bel Azur Beach Residence in Trou aux Biches

---
+++ src/data/places/mauritius/bel-azur-beach-residence-by-lov-mauritius.ts
@@ -0,0 +1,13 @@
+export default {
+  id: "bel-azur-beach-residence-by-lov-mauritius",
+  title: "Bel Azur Beach Residence by LOV Mauritius",
+  type: "hotel",
+  status: "included",
+  parentDestinationId: "trou-aux-biches",
+  description: "Hotel in Trou aux Biches, Mauritius.",
+  tripadvisorUrl:
+    "https://www.tripadvisor.se/Hotel_Review-g15591605-d1556783-Reviews-Bel_Azur_Beach_Residence_by_LOV_Mauritius-Pointe_aux_Biches.html",
+  googleMapsUrl: "https://maps.app.goo.gl/UsEUUJq35ZekFUiK7",
+  latitude: -20.0115,
+  longitude: 57.5559,
+};
