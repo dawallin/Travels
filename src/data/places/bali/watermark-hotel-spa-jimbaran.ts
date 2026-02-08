bf3d7a8d2215cae171ad149501bb94227726fdf1
Daniel Wallin <dawallin@users.noreply.github.com>
2026-02-08 15:08:15 +0100

    Add Jimbaran Watermark hotel stop

---
+++ src/data/places/bali/watermark-hotel-spa-jimbaran.ts
@@ -0,0 +1,15 @@
+export default {
+  id: "watermark-hotel-spa-jimbaran",
+  title: "Watermark Hotel & Spa Bali Jimbaran",
+  type: "hotel",
+  status: "included",
+  parentDestinationId: "jimbaran",
+  description:
+    "Hotel in the Jimbaran/Kedonganan area, close to the airport, nearby beaches, and seafood restaurants for a beach-focused final stop.",
+  address: "Jimbaran / Kedonganan, South Kuta, Bali, Indonesia",
+  tripadvisorUrl:
+    "https://www.tripadvisor.se/Hotel_Review-g3561989-d8113334-Reviews-Watermark_Hotel_Spa_Jimbaran-Kedonganan_Jimbaran_South_Kuta_Bali.html",
+  googleMapsUrl: "https://maps.app.goo.gl/xkrq1BuY6MqhY32i6",
+  latitude: -8.75377,
+  longitude: 115.17789,
+};
