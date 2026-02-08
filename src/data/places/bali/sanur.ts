e24666f0257181ae59aee2eb433b864e4e03f19e
Daniel Wallin <dawallin@users.noreply.github.com>
2026-02-08 14:57:14 +0100

    Add Sanur destination data

---
+++ src/data/places/bali/sanur.ts
@@ -0,0 +1,11 @@
+export default {
+  id: "sanur",
+  title: "Sanur",
+  type: "destination",
+  status: "included",
+  parentDestinationId: "bali",
+  description:
+    "Calm coastal town suited for rest and as a logistical base for boat trips to Nusa Penida or Nusa Lembongan.",
+  latitude: -8.693,
+  longitude: 115.263,
+};
