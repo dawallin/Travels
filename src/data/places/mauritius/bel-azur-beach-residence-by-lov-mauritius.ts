bb3fbc5ca2da3c745062d79dacc1c2cf33436fce
Daniel Wallin <dawallin@users.noreply.github.com>
2026-02-08 21:06:49 +0100

    Update Bel Azur Beach Residence coordinates

--- src/data/places/mauritius/bel-azur-beach-residence-by-lov-mauritius.ts
+++ src/data/places/mauritius/bel-azur-beach-residence-by-lov-mauritius.ts
@@ -8,6 +8,6 @@ export default {
   tripadvisorUrl:
     "https://www.tripadvisor.se/Hotel_Review-g15591605-d1556783-Reviews-Bel_Azur_Beach_Residence_by_LOV_Mauritius-Pointe_aux_Biches.html",
   googleMapsUrl: "https://maps.app.goo.gl/UsEUUJq35ZekFUiK7",
-  latitude: -20.0115,
-  longitude: 57.5559,
+  latitude: -20.0461,
+  longitude: 57.531083,
 };
