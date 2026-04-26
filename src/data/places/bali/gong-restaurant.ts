5f0e7dd0d5d044fa7a71b2d82d8133209ccb8880
Daniel Wallin <dawallin@users.noreply.github.com>
2026-04-26 17:13:26 +0200

    Add Gong Restaurant place entry for Sanur Bali

---
+++ src/data/places/bali/gong-restaurant.ts
@@ -0,0 +1,23 @@
+export default {
+  id: "gong-restaurant",
+  title: "Gong Restaurant Indonesian and Balinese Cuisine",
+  type: "restaurant",
+  status: "possible",
+  parentDestinationId: "sanur",
+  description: "Restaurant in Sanur, Denpasar, Bali.",
+  details: {
+    what: "Indonesian and Balinese cuisine restaurant in Sanur.",
+  },
+  address: "Jl. Tirta Akasa No.28, Sanur, Denpasar, Bali, Indonesia",
+  googleMapsUrl: "https://maps.app.goo.gl/fCWJr57MFT8QrB98A?g_st=ic",
+  tripadvisorUrl:
+    "https://www.tripadvisor.se/Restaurant_Review-g297700-d1994612-Reviews-Gong_Restaurant_Indonesian_and_Balinese_Cuisine-Sanur_Denpasar_Bali.html",
+  links: {
+    official: "https://gong-restaurant.com",
+    tripAdvisor:
+      "https://www.tripadvisor.se/Restaurant_Review-g297700-d1994612-Reviews-Gong_Restaurant_Indonesian_and_Balinese_Cuisine-Sanur_Denpasar_Bali.html",
+    googleMaps: "https://maps.app.goo.gl/fCWJr57MFT8QrB98A?g_st=ic",
+  },
+  latitude: -8.6994178,
+  longitude: 115.2573352,
+};
