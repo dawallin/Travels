fe39947e37d2cc3b753d6eb3d11d76b865b15e07
Daniel Wallin <dawallin@users.noreply.github.com>
2026-06-06 00:12:44 +0200

    Add Ho Chi Minh shopping and post office places

---
+++ src/data/places/vietnam/dong-khoi-street.ts
@@ -0,0 +1,16 @@
+export default {
+  id: "dong-khoi-street",
+  title: "Đồng Khởi Street",
+  type: "shopping",
+  status: "possible",
+  parentDestinationId: "ho-chi-minh-city",
+  description: "Shopping street in Ho Chi Minh City.",
+  details: {
+    what: "Shopping street in Ho Chi Minh City.",
+  },
+  links: {
+    googleMaps: "https://www.google.com/maps?q=10.7760613,106.7030772",
+  },
+  latitude: 10.7760613,
+  longitude: 106.7030772,
+};
---
+++ src/data/places/vietnam/saigon-central-post-office.ts
@@ -0,0 +1,16 @@
+export default {
+  id: "saigon-central-post-office",
+  title: "Saigon Central Post Office",
+  type: "attraction",
+  status: "possible",
+  parentDestinationId: "ho-chi-minh-city",
+  description: "Post office in Ho Chi Minh City.",
+  details: {
+    what: "Post office in Ho Chi Minh City.",
+  },
+  links: {
+    googleMaps: "https://www.google.com/maps?q=10.7799129,106.699902",
+  },
+  latitude: 10.7799129,
+  longitude: 106.699902,
+};
