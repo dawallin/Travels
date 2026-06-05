8018acb5167094d443cf1b57798c416746c75c90
Daniel Wallin <dawallin@users.noreply.github.com>
2026-06-05 23:25:00 +0200

    Add second Ho Chi Minh Oasis cafe

---
+++ src/data/places/vietnam/oasis-cafe.ts
@@ -0,0 +1,16 @@
+export default {
+  id: "oasis-cafe",
+  title: "Oasis Cafe",
+  type: "restaurant",
+  status: "possible",
+  parentDestinationId: "ho-chi-minh-city",
+  description: "Possible cafe stop in Ho Chi Minh City.",
+  details: {
+    what: "Possible cafe stop in Ho Chi Minh City.",
+  },
+  links: {
+    googleMaps: "https://www.google.com/maps?q=10.7927268,106.6710981",
+  },
+  latitude: 10.7927268,
+  longitude: 106.6710981,
+};
